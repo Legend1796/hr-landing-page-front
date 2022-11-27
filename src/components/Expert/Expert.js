f371b6ad98780f60ecfef8c26f8fbfe8cbbac307
Igor Manukhov <m_igor97@mail.ru>
2022-11-27 22:47:46 +0300

    added fonts and done expert

--- src/components/Expert/Expert.js
+++ src/components/Expert/Expert.js
@@ -1,4 +1,7 @@
-// import promoImage from '../../images/Vector37.svg';
+import firsdtStep from '../../images/1step.svg';
+import secondtStep from '../../images/2step.svg';
+import thirdStep from '../../images/3step.svg';
+import expertImage from '../../images/expert.svg';
 
 function Expert() {
 
@@ -6,20 +9,20 @@ function Expert() {
     <section className='expert'>
       <h2 className='expert__title'>Как стать экспертом</h2>
       <ul className='expert__cards'>
-        <li className='expert__card'>
-          <h3 className='expert__card-title'>ШАГ 1</h3>
+        <li className='expert__card expert__card_first'>
+          <img className='expert__card-title' src={firsdtStep} alt='1 шаг' />
           <p className='expert__card-paragraph'>Выберите роль и заполните заявку</p>
         </li>
-        <li className='expert__card'>
-          <h3 className='expert__card-title'>ШАГ 2</h3>
+        <li className='expert__card expert__card_second'>
+          <img className='expert__card-title' src={secondtStep} alt='2 шаг' />
           <p className='expert__card-paragraph'>Пройдите отбор и бесплатное обучение</p>
         </li>
-        <li className='expert__card'>
-          <h3 className='expert__card-title'>ШАГ 3</h3>
+        <li className='expert__card expert__card_third'>
+          <img className='expert__card-title' src={thirdStep} alt='3 шаг' />
           <p className='expert__card-paragraph'>Делитесь знаниями со студентами Яндекс Практикума</p>
         </li>
       </ul>
-
+      <img className='expert__bottom-image' src={expertImage} alt='Картинка пути по шагам' />
     </section>
   )
 }
