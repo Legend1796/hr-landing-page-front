66530c0033b8e51b7712cd6607d1df33897acec0
Igor Manukhov <m_igor97@mail.ru>
2022-11-29 11:58:50 +0300

    feedback done buttons

--- src/components/FeedBack/FeedBack.js
+++ src/components/FeedBack/FeedBack.js
@@ -1,17 +1,21 @@
 import videoOne from '../../images/feedback-video-image.png';
 import next from '../../images/feedback-next.svg';
 import previous from '../../images/feedback-previous.svg';
+import play from '../../images/play-feedback.svg';
 
 function FeedBack() {
 
   return (
-    <feedback className='feedback'>
+    <section className='feedback'>
+      {/* <div className='feedback__video-container'> */}
+      <h3 className='feedback__title'>Отзывы</h3>
       <div className='feedback__video-container'>
-        <h3 className='feedback__title'>Отзывы</h3>
-        <video className='feedback__video' poster={videoOne}>
-        </video>
-        <div className='feedback__video-front'>
-          <p className='feedback__text'>«Всё моё наставничество — это челлендж»</p>
+        <img className='feedback__video' src={videoOne} alt='Постер отзыва' />
+        <img className='feedback__video-play' src={play} alt='play' />
+      </div>
+      <div className='feedback__video-front'>
+        <p className='feedback__text'>«Всё моё наставничество — это челлендж»</p>
+        <div className='feedback__buttons'>
           <button className='feedback__send-request' type='button'>
             <img className='feedback__link' src={previous} alt='Предыдущее видео' />
           </button>
@@ -19,8 +23,9 @@ function FeedBack() {
             <img className='feedback__link' src={next} alt='Следующее видео' />
           </button>
         </div>
+        {/* </div> */}
       </div>
-    </feedback >
+    </section >
   )
 }
 
--- src/components/Promo/Promo.js
+++ src/components/Promo/Promo.js
@@ -4,24 +4,49 @@ function Promo() {
   return (
     <section className='promo'>
       <h1 className='promo__title'>Делись своим опытом</h1>
-      <div className='promo__line promo__line_first'>
-        <p className='promo__paragrapf promo__paragrapf_big'>дизайне анализе данных программировании</p>
-        <p className='promo__paragrapf promo__paragrapf_big'>дизайне анализе данных программировании</p>
-
-        {/* <p className='promo__paragrapf promo__paragrapf_big'>дизайне</p>
-        <p className='promo__paragrapf promo__paragrapf_blue-huge'>анализе данных</p>
-        <p className='promo__paragrapf promo__paragrapf_blue-big'>программировании</p> */}
-      </div>
-      <div className='promo__line promo__line_second'>
-        <p className='promo__paragrapf promo__paragrapf_big'>программировании</p>
-        <p className='promo__paragrapf promo__paragrapf_small'>менеджменте</p>
-        <p className='promo__paragrapf promo__paragrapf_big'>маркетинге</p>
-      </div>
-      <div className='promo__line promo__line_third'>
-        <p className='promo__paragrapf promo__paragrapf_blue-smallest'>менеджменте</p>
-        <p className='promo__paragrapf promo__paragrapf_big'>программировании</p>
-        <p className='promo__paragrapf promo__paragrapf_blue-big'>маркетинге</p>
-        <p className='promo__paragrapf promo__paragrapf_blue-small'>дизайне</p>
+      <div className='promo__line'>
+        <div className='promo__line_first'>
+          <h3 className='promo__paragrapf'>
+            <p className='promo__text'>программировании</p>
+            <p className='promo__text'>дизайне</p>
+            <p className='promo__text'>анализе</p>
+            <p className='promo__text'>данных</p>
+          </h3>
+          <h3 className='promo__paragrapf'>
+            <p className='promo__text'>программировании</p>
+            <p className='promo__text'>дизайне</p>
+            <p className='promo__text'>анализе</p>
+            <p className='promo__text'>данных</p>
+          </h3>
+        </div>
+        <div className='promo__line_first'>
+          <h3 className='promo__paragrapf'>
+            <p className='promo__text'>программировании</p>
+            <p className='promo__text'>дизайне</p>
+            <p className='promo__text'>анализе</p>
+            <p className='promo__text'>данных</p>
+          </h3>
+          <h3 className='promo__paragrapf'>
+            <p className='promo__text'>программировании</p>
+            <p className='promo__text'>дизайне</p>
+            <p className='promo__text'>анализе</p>
+            <p className='promo__text'>данных</p>
+          </h3>
+        </div>
+        <div className='promo__line_first'>
+          <h3 className='promo__paragrapf'>
+            <p className='promo__text'>программировании</p>
+            <p className='promo__text'>дизайне</p>
+            <p className='promo__text'>анализе</p>
+            <p className='promo__text'>данных</p>
+          </h3>
+          <h3 className='promo__paragrapf'>
+            <p className='promo__text'>программировании</p>
+            <p className='promo__text'>дизайне</p>
+            <p className='promo__text'>анализе</p>
+            <p className='promo__text'>данных</p>
+          </h3>
+        </div>
       </div>
       <div className='promo__bottom-сontainer'>
         <button className='promo__buttom' type='button'>Хочу делиться знаниями</button>
