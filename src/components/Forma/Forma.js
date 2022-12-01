dc59903c7f395b9235a06976b6ced89a4175a284
Egor Talovikov <egtalovikov@gmail.com>
2022-12-01 17:58:22 +0300

    feat: добавил селектор

--- src/components/Forma/Forma.js
+++ src/components/Forma/Forma.js
@@ -1,6 +1,50 @@
+/* eslint-disable no-console */
+import React from 'react';
 import { useForm } from 'react-hook-form';
+import Dropdown from 'rc-dropdown';
+import 'rc-dropdown/assets/index.css';
+import Menu, { Item as MenuItem, Divider } from 'rc-menu';
+import ReactDOM from 'react-dom';
 
 const Forma = () => {
+
+    const [buttonTitle, setButtonTitle] = React.useState('Направление')
+    const [selectorImage, setSelectorImage] = React.useState(<svg width="11" height="7" viewBox="0 0 11 7" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M1.5 1.71094L5.5 5.71094L9.5 1.71094" stroke="#1D6BF3" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+    </svg>)
+
+    const menu = (
+        <Menu onSelect={onSelect}>
+            <MenuItem key="Программирование">Программирование</MenuItem>
+            <Divider />
+            <MenuItem key="Анализ данных">Анализ данных</MenuItem>
+            <Divider />
+            <MenuItem key="Дизайн">Дизайн</MenuItem>
+            <Divider />
+            <MenuItem key="Маркетинг">Маркетинг</MenuItem>
+            <Divider />
+            <MenuItem key="Менеджмент">Менеджмент</MenuItem>
+        </Menu>
+    );
+
+    function onSelect({ key }) {
+        setButtonTitle(key);
+    }
+
+    function onVisibleChange(visible) {
+        if (visible) {
+            setSelectorImage(<svg width="10" height="6" viewBox="0 0 10 6" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M9 5L5 1L1 5" stroke="#1D6BF3" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+            </svg>
+            )
+        } else {
+            setSelectorImage(<svg width="11" height="7" viewBox="0 0 11 7" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M1.5 1.71094L5.5 5.71094L9.5 1.71094" stroke="#1D6BF3" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+            </svg>)
+        }
+    }
+
+
     const {
         register,
         formState: { errors, isDirty, isValid },
@@ -15,7 +59,7 @@ const Forma = () => {
         <section className="formaSauvka">
             <div className="formaSauvka__container">
                 <h2 className="formaSauvka__title">Оставьте заявку и мы свяжемся с вами</h2>
-                    <form className="formaSauvka__forma" onSubmit={handleSubmit(onSubmit)}>
+                <form className="formaSauvka__forma" onSubmit={handleSubmit(onSubmit)}>
                     <div className='formaSauvka__form-container'>
                         <div className='formaSauvka__field'>
                             <input className="formaSauvka__inputs formaSauvka__inputs_1"
@@ -37,7 +81,7 @@ const Forma = () => {
                                 placeholder="* Имя"
                                 type="text"
                             />
-                            {errors?.name && <span className="formaSauvka__text-error">{errors.name.message}</span>}
+                            <span className={`formaSauvka__text-error ${errors.name ? 'formaSauvka__text-error_visible' : ''}`}>{errors.name ? errors.name.message : ''}</span>
                         </div>
                         <div className="formaSauvka__field">
                             <input className="formaSauvka__inputs formaSauvka__inputs_2"
@@ -53,17 +97,24 @@ const Forma = () => {
                             />
                             <span className={`formaSauvka__text-error ${errors.telegram ? 'formaSauvka__text-error_visible' : ''}`}>{errors.telegram ? errors.telegram.message : ''}</span>
                         </div>
+                    </div>
+                    <div className="formaSauvka__form-container">
+                        <div>
+                            <div>
+                                <div>
+                                    <Dropdown
+                                        trigger={['click']}
+                                        overlay={menu}
+                                        animation="slide-up"
+                                        onVisibleChange={onVisibleChange}
+                                    >
+                                        <button className='formaSauvka__direction-button'>{buttonTitle}
+                                            {selectorImage}
+                                        </button>
+                                    </Dropdown>
+                                </div>
+                            </div>
                         </div>
-                        <div className="formaSauvka__form-container">
-                        <select className="formaSauvka__field formaSauvka__field_select" name="profi" id="profi-select">
-                            <option value="">* Направление</option>
-                            <option className="formaSauvka__profis" value="programm">Программирование</option>
-                            <option className="formaSauvka__profis" value="analiz">Анализ данных</option>
-                            <option className="formaSauvka__profis" value="dizain">Дизайн</option>
-                            <option className="formaSauvka__profis" value="marketing">Маркетинг</option>
-                            <option className="formaSauvka__profis" value="menegment">Менеджмент</option>
-                        </select>
-
                         <div className='formaSauvka__field'>
                             <input className="formaSauvka__inputs formaSauvka__inputs_3"
                                 {...register('portfolio', {
@@ -76,29 +127,29 @@ const Forma = () => {
                                 type="text"
                                 placeholder='* Ссылка на портфолио'
                             />
-                            {errors?.portfolio && <span className="formaSauvka__text-error">{errors.portfolio.message}</span>}
+                            <span className={`formaSauvka__text-error ${errors.portfolio ? 'formaSauvka__text-error_visible' : ''}`}>{errors.portfolio ? errors.portfolio.message : ''}</span>
                         </div>
-                        </div>
+                    </div>
+                    <textarea id="user-stek" className="formaSauvka__inputs_stek"
+                        {...register('stek', {
+                            required: "Нужно заполнить"
+                        })}
+                        rows="5"
+                        cols="60"
+                        placeholder='Навыки и инструменты'>
+                    </textarea>
+                    <span className={`formaSauvka__text-error ${errors.stek ? 'formaSauvka__text-error_visible' : ''}`}>{errors.stek ? errors.stek.message : ''}</span>
 
-                        <textarea id="user-stek" className="formaSauvka__inputs_stek"
-                            {...register('stek', {
-                                required: "Нужно заполнить"
-                            })}
-                            rows="5"
-                            cols="60"
-                            placeholder='Навыки и инструменты'>
-                        </textarea>
-                        {errors?.stek && <span className="formaSauvka__text_error">{errors.stek.message}</span>}
+                    <div className="formaSauvka__checkbox">
+                        <label className="custom-checkbox">
+                            <span className='star'>*</span>
+                            <input className='formaSauvka__checkbox-item' type="checkbox" name="soglacie" value="yes" />
+                            <span>Я даю согласие АНО ДПО «ШАД» и ООО «ЯНДЕКС» на обработку указанных данных для целей рассмотрения анкеты и обратной связи по ней.</span>
+                        </label>
+                    </div>
 
-                        <div className="formaSauvka__checkbox">
-                            <label className="custom-checkbox">
-                                <input type="checkbox" name="soglacie" value="yes" />
-                                <span>Я даю согласие АНО ДПО «ШАД» и ООО «ЯНДЕКС» на обработку указанных данных для целей рассмотрения анкеты и обратной связи по ней.</span>
-                            </label>
-                        </div>
-
-                        <button className="formaSauvka__submit-button" type="submit" >Отправить заявку</button>
-                    </form>
+                    <button className="formaSauvka__submit-button" type="submit" >Отправить заявку</button>
+                </form>
             </div>
         </section>
     );
