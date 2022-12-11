84a52a5a8040fbd6f3e53cb3140fd6973cdd083f
Igor Manukhov <m_igor97@mail.ru>
2022-12-11 15:02:44 +0300

    hiddden buttons popup

--- src/components/Popup/Popup.js
+++ src/components/Popup/Popup.js
@@ -7,32 +7,32 @@ function Popup({ isOpen, onClose, title, btnId, activateButton, texts }) {
 
   function activeButton1() {
     activateButton(1);
-    moveButtons.style = 'left: 10';
+    // moveButtons.style = 'left: 10';
   }
 
   function activeButton2() {
     activateButton(2);
-    moveButtons.style = 'left: -67px';
+    // moveButtons.style = 'left: -67px';
   }
 
   function activeButton3() {
     activateButton(3);
-    moveButtons.style = 'left: -305px';
+    // moveButtons.style = 'left: -305px';
   }
 
   function activeButton4() {
     activateButton(4);
-    moveButtons.style = 'left: -565px';
+    // moveButtons.style = 'left: -565px';
   }
 
   function activeButton5() {
     activateButton(5);
-    moveButtons.style = 'left: -825px';
+    // moveButtons.style = 'left: -825px';
   }
 
   function activeButton6() {
     activateButton(6);
-    moveButtons.style = 'left: -1035px';
+    // moveButtons.style = 'left: -1035px';
   }
 
 
@@ -46,27 +46,27 @@ function Popup({ isOpen, onClose, title, btnId, activateButton, texts }) {
         </div>
         <div className='popup__two-columns'>
           <div className='popup__buttons'>
-            <div className='popup__move-buttons' id='popup__move-buttons'>
-              <button onClick={activeButton1} className={`popup__button ${btnId === 1 ? 'popup__button_active' : ''}`} type='button' aria-label='Кнопка чата'>
-                <p className='popup__button-name'>О чем курс?</p>
-              </button>
-              <button onClick={activeButton2} className={`popup__button ${btnId === 2 ? 'popup__button_active' : ''}`} type='button' aria-label='Кнопка чата'>
-                <p className='popup__button-name'>Что делает наставник?</p>
-              </button>
-              <button onClick={activeButton3} className={`popup__button ${btnId === 3 ? 'popup__button_active' : ''}`} type='button' aria-label='Кнопка чата'>
-                <p className='popup__button-name'>Я хочу стать наставником. Меня возьмут?</p>
-              </button>
-              <button onClick={activeButton4} className={`popup__button ${btnId === 4 ? 'popup__button_active' : ''}`} type='button' aria-label='Кнопка чата'>
-                <p className='popup__button-name'>Я подхожу. Что вы можете мне предложить?</p>
-              </button>
-              <button onClick={activeButton5} className={`popup__button ${btnId === 5 ? 'popup__button_active' : ''}`} type='button' aria-label='Кнопка чата'>
-                <p className='popup__button-name'>Как я пойму, что у меня достаточно экспертизы?</p>
-              </button>
-              <button onClick={activeButton6} className={`popup__button ${btnId === 6 ? 'popup__button_active' : ''}`} type='button' aria-label='Кнопка чата'>
-                <p className='popup__button-name'>Что такое Школа наставников и сколько времени она занимает?</p>
-              </button>
-            </div>
+            {/* <div className='popup__move-buttons' id='popup__move-buttons'> */}
+            <button onClick={activeButton1} className={`popup__button ${btnId === 1 ? 'popup__button_active' : ''}`} type='button' aria-label='Кнопка чата'>
+              <p className='popup__button-name'>О чем курс?</p>
+            </button>
+            <button onClick={activeButton2} className={`popup__button ${btnId === 2 ? 'popup__button_active' : ''}`} type='button' aria-label='Кнопка чата'>
+              <p className='popup__button-name'>Что делает наставник?</p>
+            </button>
+            <button onClick={activeButton3} className={`popup__button ${btnId === 3 ? 'popup__button_active' : ''}`} type='button' aria-label='Кнопка чата'>
+              <p className='popup__button-name'>Я хочу стать наставником. Меня возьмут?</p>
+            </button>
+            <button onClick={activeButton4} className={`popup__button ${btnId === 4 ? 'popup__button_active' : ''}`} type='button' aria-label='Кнопка чата'>
+              <p className='popup__button-name'>Я подхожу. Что вы можете мне предложить?</p>
+            </button>
+            <button onClick={activeButton5} className={`popup__button ${btnId === 5 ? 'popup__button_active' : ''}`} type='button' aria-label='Кнопка чата'>
+              <p className='popup__button-name'>Как я пойму, что у меня достаточно экспертизы?</p>
+            </button>
+            <button onClick={activeButton6} className={`popup__button ${btnId === 6 ? 'popup__button_active' : ''}`} type='button' aria-label='Кнопка чата'>
+              <p className='popup__button-name'>Что такое Школа наставников и сколько времени она занимает?</p>
+            </button>
           </div>
+          {/* </div> */}
           <div className='popup__texts'>
             {texts.map((text) => (
               <Text key={text.id} text={text.text} id={text.id} />
