f0d89fe04ea7d90ffc05ac6927a35c79505cae8f
Igor Manukhov <m_igor97@mail.ru>
2022-12-13 20:38:29 +0300

    change css class form

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -18,17 +18,17 @@ const Form = ({ formRef }) => {
   </svg>)
 
   const menu = (
-    <Menu onSelect={onSelect} className='formaSauvka__menu'>
-      <Divider className='formaSauvka__divider' />
-      <MenuItem className="formaSauvka__menu-item" key="Программирование">Программирование</MenuItem>
-      <Divider className='formaSauvka__divider' />
-      <MenuItem className="formaSauvka__menu-item" key="Анализ данных">Анализ данных</MenuItem>
-      <Divider className='formaSauvka__divider' />
-      <MenuItem className="formaSauvka__menu-item" key="Дизайн">Дизайн</MenuItem>
-      <Divider className='formaSauvka__divider' />
-      <MenuItem className="formaSauvka__menu-item" key="Маркетинг">Маркетинг</MenuItem>
-      <Divider className='formaSauvka__divider' />
-      <MenuItem className="formaSauvka__menu-item" key="Менеджмент">Менеджмент</MenuItem>
+    <Menu onSelect={onSelect} className='form__menu'>
+      <Divider className='form__divider' />
+      <MenuItem className="form__menu-item" key="Программирование">Программирование</MenuItem>
+      <Divider className='form__divider' />
+      <MenuItem className="form__menu-item" key="Анализ данных">Анализ данных</MenuItem>
+      <Divider className='form__divider' />
+      <MenuItem className="form__menu-item" key="Дизайн">Дизайн</MenuItem>
+      <Divider className='form__divider' />
+      <MenuItem className="form__menu-item" key="Маркетинг">Маркетинг</MenuItem>
+      <Divider className='form__divider' />
+      <MenuItem className="form__menu-item" key="Менеджмент">Менеджмент</MenuItem>
     </Menu>
   );
 
@@ -73,13 +73,13 @@ const Form = ({ formRef }) => {
   }
 
   return (
-    <section className="formaSauvka" ref={formRef} id='send'>
-      <div className="formaSauvka__container">
-        <h2 className="formaSauvka__title">Оставьте заявку и мы свяжемся с вами</h2>
-        <form className="formaSauvka__forma" onSubmit={handleSubmit(onSubmit)}>
-          <div className='formaSauvka__form-container'>
-            <div onChange={handleNameInputChange} className='formaSauvka__field'>
-              <input value={nameInputValue} className={`${errors.name ? "formaSauvka__inputs_error" : ''} formaSauvka__inputs formaSauvka__inputs_1 ${(!errors.name && nameInputValue != '') ? 'formaSauvka__inputs_typed' : ''}`}
+    <section className="form" ref={formRef} id='send'>
+      <div className="form__container">
+        <h2 className="form__title">Оставьте заявку и мы свяжемся с вами</h2>
+        <form className="form__forma" onSubmit={handleSubmit(onSubmit)}>
+          <div className='form__form-container'>
+            <div onChange={handleNameInputChange} className='form__field'>
+              <input value={nameInputValue} className={`${errors.name ? "form__inputs_error" : ''} form__inputs form__inputs_1 ${(!errors.name && nameInputValue != '') ? 'form__inputs_typed' : ''}`}
                 {...register("name", {
                   minLength: {
                     value: 2,
@@ -98,10 +98,10 @@ const Form = ({ formRef }) => {
                 placeholder="* Имя"
                 type="text"
               />
-              <span className={`formaSauvka__text-error ${errors.name ? 'formaSauvka__text-error_visible' : ''}`}>{errors.name ? errors.name.message : ''}</span>
+              <span className={`form__text-error ${errors.name ? 'form__text-error_visible' : ''}`}>{errors.name ? errors.name.message : ''}</span>
             </div>
-            <div onChange={handleTelegramInputChange} className="formaSauvka__field">
-              <input value={telegramInputValue} className={`${errors.telegram ? "formaSauvka__inputs_error" : ''} formaSauvka__inputs formaSauvka__inputs_2 ${(!errors.telegram && telegramInputValue != '') ? 'formaSauvka__inputs_typed' : ''}`}
+            <div onChange={handleTelegramInputChange} className="form__field">
+              <input value={telegramInputValue} className={`${errors.telegram ? "form__inputs_error" : ''} form__inputs form__inputs_2 ${(!errors.telegram && telegramInputValue != '') ? 'form__inputs_typed' : ''}`}
                 {...register('telegram', {
                   pattern: {
                     value: /@([A-Za-z0-9_]{1,15})/,
@@ -112,10 +112,10 @@ const Form = ({ formRef }) => {
                 type="text"
                 placeholder="* Телеграм"
               />
-              <span className={`formaSauvka__text-error ${errors.telegram ? 'formaSauvka__text-error_visible' : ''}`}>{errors.telegram ? errors.telegram.message : ''}</span>
+              <span className={`form__text-error ${errors.telegram ? 'form__text-error_visible' : ''}`}>{errors.telegram ? errors.telegram.message : ''}</span>
             </div>
           </div>
-          <div className="formaSauvka__form-container">
+          <div className="form__form-container">
             <div>
               <div>
                 <div>
@@ -125,15 +125,15 @@ const Form = ({ formRef }) => {
                     animation="slide-up"
                     onVisibleChange={onVisibleChange}
                   >
-                    <button className={`formaSauvka__direction-button ${buttonTitle != 'Направление' ? 'formaSauvka__direction-button_active' : ''}`}>{buttonTitle}
+                    <button className={`form__direction-button ${buttonTitle != 'Направление' ? 'form__direction-button_active' : ''}`}>{buttonTitle}
                       {selectorImage}
                     </button>
                   </Dropdown>
                 </div>
               </div>
             </div>
-            <div onChange={handlePortfolioInputChange} className='formaSauvka__field'>
-              <input className={`${errors.portfolio ? "formaSauvka__inputs_error" : ''} formaSauvka__inputs formaSauvka__inputs_3 ${(!errors.portfolio && portfolioInputValue != '') ? 'formaSauvka__inputs_typed' : ''}`}
+            <div onChange={handlePortfolioInputChange} className='form__field'>
+              <input className={`${errors.portfolio ? "form__inputs_error" : ''} form__inputs form__inputs_3 ${(!errors.portfolio && portfolioInputValue != '') ? 'form__inputs_typed' : ''}`}
                 {...register('portfolio', {
                   pattern: {
                     value: /[-a-zA-Z0-9@:%_\+.~#?&\/=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&\/=]*)?/gi,
@@ -144,10 +144,10 @@ const Form = ({ formRef }) => {
                 type="text"
                 placeholder='* Ссылка на портфолио'
               />
-              <span className={`formaSauvka__text-error ${errors.portfolio ? 'formaSauvka__text-error_visible' : ''}`}>{errors.portfolio ? errors.portfolio.message : ''}</span>
+              <span className={`form__text-error ${errors.portfolio ? 'form__text-error_visible' : ''}`}>{errors.portfolio ? errors.portfolio.message : ''}</span>
             </div>
           </div>
-          <textarea id="user-stek" className="formaSauvka__inputs_stek"
+          <textarea id="user-stek" className="form__inputs_stek"
             {...register('stek', {
               required: "Нужно заполнить"
             })}
@@ -155,15 +155,15 @@ const Form = ({ formRef }) => {
             cols="60"
             placeholder='Навыки и инструменты'>
           </textarea>
-          <span className={`formaSauvka__text-error ${errors.stek ? 'formaSauvka__text-error_visible' : ''}`}>{errors.stek ? errors.stek.message : ''}</span>
+          <span className={`form__text-error ${errors.stek ? 'form__text-error_visible' : ''}`}>{errors.stek ? errors.stek.message : ''}</span>
 
-          <div className="formaSauvka__checkbox">
+          <div className="form__checkbox">
             <span className='star'>*</span>
-            <input className='formaSauvka__checkbox-item' type="checkbox" name="soglacie" value="yes" />
-            <span className='formaSauvka__checkbox-text'>Я даю согласие АНО ДПО «ШАД» и ООО «ЯНДЕКС» на обработку указанных данных для целей рассмотрения анкеты и обратной связи по ней.</span>
+            <input className='form__checkbox-item' type="checkbox" name="soglacie" value="yes" />
+            <span className='form__checkbox-text'>Я даю согласие АНО ДПО «ШАД» и ООО «ЯНДЕКС» на обработку указанных данных для целей рассмотрения анкеты и обратной связи по ней.</span>
           </div>
 
-          <button className="formaSauvka__submit-button" type="submit" >Отправить заявку</button>
+          <button className="form__submit-button" type="submit" >Отправить заявку</button>
         </form>
       </div>
     </section>
